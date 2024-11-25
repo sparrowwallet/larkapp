bca82ef73e96be4ba503d9b37d9c70cf7fd023ff
Craig Raw <craigraw@gmail.com>
2024-11-25 17:37:04 +0200

    add module info

---
+++ src/main/java/module-info.java
@@ -0,0 +1,7 @@
+module com.sparrowwallet.larkapp {
+    requires com.sparrowwallet.lark;
+    requires com.sparrowwallet.drongo;
+    requires com.fasterxml.jackson.databind;
+    requires org.jcommander;
+    requires org.slf4j;
+}
